8c73c82b523fb9b4562ef74466294eb0cb69237f
jdi022222 <jdi022222@gmail.com>
2023-10-18 22:16:41 +0900

    feat: Redis를 통한 race condition 해결

--- api/src/main/java/com/example/api/repository/CouponRepository.java
+++ api/src/main/java/com/example/api/repository/CouponRepository.java
@@ -1,8 +1,10 @@
 package com.example.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.api.domain.Coupon;
 
+@Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 }
