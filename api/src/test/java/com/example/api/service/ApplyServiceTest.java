85aa2b2dc9ae76c50c0eee26f073db87ab3d3124
jdi022222 <jdi022222@gmail.com>
2023-10-18 21:44:35 +0900

    feat: 간단한 쿠폰 발급 로직 구현

---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.api.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.api.repository.CouponRepository;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+	@Autowired
+	private ApplyService applyService;
+
+	@Autowired
+	private CouponRepository couponRepository;
+
+	@Test
+	public void 한번만응모() {
+		applyService.apply(1L);
+
+		long count = couponRepository.count();
+
+		Assertions.assertThat(count).isEqualTo(1);
+	}
+}
