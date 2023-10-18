40858f41e10f8916cdfccc3562e0c65183e864ac
jdi022222 <jdi022222@gmail.com>
2023-10-18 21:59:33 +0900

    test: 병렬적으로 쿠폰 발급을 요청할 경우 race condition이 발생하는 테스트 작성

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -1,5 +1,9 @@
 package com.example.api.service;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +21,37 @@ class ApplyServiceTest {
 	private CouponRepository couponRepository;
 
 	@Test
-	public void 한번만응모() {
+	public void 한번만응모() throws Exception{
 		applyService.apply(1L);
 
 		long count = couponRepository.count();
 
 		Assertions.assertThat(count).isEqualTo(1);
 	}
+
+	@Test
+	public void 여러명응모() throws Exception {
+		int threadCount = 1000;
+		ExecutorService executorService = Executors.newFixedThreadPool(32); // 병렬 작업을 도와주는 java api
+		CountDownLatch countDownLatch = new CountDownLatch(threadCount); // 다른 스레드의 작업이 끝날 때까지 기다리는 클래스
+
+		for (int i = 0; i < threadCount; i++) {
+			long userId = i;
+			executorService.submit(() -> {
+					try {
+						applyService.apply(userId);
+					} finally {
+						countDownLatch.countDown();
+					}
+				}
+			);
+		}
+
+		countDownLatch.await();
+
+		long count = couponRepository.count();
+
+		Assertions.assertThat(count).isEqualTo(100); // race condition 발생!
+	}
+
 }
